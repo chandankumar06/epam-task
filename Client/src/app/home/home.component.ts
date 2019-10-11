0f56885a9e09c6d3db62ea16afdbe7244d6574da
Chandan Kumar N A <chandan-ku@HCL.COM>
2019-10-11 10:09:24 +0530

    cover letter

--- Client/src/app/home/home.component.ts
+++ Client/src/app/home/home.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { AppService } from '.././app.service';
-// import { UserTransactionDetails } from './../interfaces/userTransactionDetails.interface';
-// import { UserDetails } from './../interfaces/userDetails.interface';
 declare var $: any;
 
 @Component({
@@ -12,9 +10,6 @@ declare var $: any;
 })
 
 export class HomeComponent implements OnInit {
-  // userTransactionDetails = new UserTransactionDetails();
-  // userDetails = new UserDetails();
-
   constructor(private appService: AppService, private router: Router) {
     console.log("HomeComponent constuctor()")
   }
